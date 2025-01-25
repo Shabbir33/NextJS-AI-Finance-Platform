b0feba2dcd7317b71ffabaf42397ab5745f2b62e
Shabbir33 <shabbirpoonawala03@gmail.com>
2025-01-25 13:31:37 +0530

    Completed Add and Edit Transaction Form, Added Rate Limiting using Arcjet, Implemented AI Receipt Scanner Component using Gemini, Implemented Recurring Transaction Innjest Function.

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,6 +1,10 @@
 import { inngest } from "@/lib/inngest/client";
 import { serve } from "inngest/next";
-import { checkBudgetAlert } from "@/lib/inngest/function";
+import {
+  checkBudgetAlert,
+  processRecurringTransactions,
+  triggerRecurringTransactions,
+} from "@/lib/inngest/function";
 
 // Create an API that serves zero functions
 export const { GET, POST, PUT } = serve({
@@ -8,5 +12,7 @@ export const { GET, POST, PUT } = serve({
   functions: [
     /* your functions will be passed here later! */
     checkBudgetAlert,
+    triggerRecurringTransactions,
+    processRecurringTransactions,
   ],
 });
